8d6fc17831efe4f02451dae77b2cf4995e1896a1
xianyanjia <jiaxianyan@gmail.com>
2022-03-01 14:07:35 +0800

    clean unused code

--- bert/print_header.py
+++
@@ -1,5 +0,0 @@
-script_header = "EPL Training Script"
-indent_len = 30
-print("{} {} {}".format("="*indent_len, script_header, "="*indent_len))
-
-print("="*(indent_len*2+len(script_header)+2))
--- resnet/scripts/train_dp_wh.sh
+++
@@ -1,4 +0,0 @@
-python resnet_dp_whale.py \
-    --gc=1 \
-    --amp=1 \
-    --zero=1
